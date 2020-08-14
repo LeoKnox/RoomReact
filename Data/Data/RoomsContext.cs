888c4e6d5c85a09388b3b62ad8461ca43700fc81
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-13 19:22:18 -0700

    Add files via upload

---
+++ Data/Data/RoomsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactRoom.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRoom.Data.Data
+{
+    public class RoomsContext : DbContext
+    {
+        public DbSet<Room> Room { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            DbContextOptionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=")
+        }
+    }
+}
