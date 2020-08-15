77be0dc6495b4d385a3994a7468f6ada5c94412c
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-14 18:14:37 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ReactRoom.Data;
+using ReactRoom.Data.Data;
 
 namespace ReactRoom
 {
@@ -21,6 +23,7 @@ namespace ReactRoom
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<RoomsContext>(options => options.UseSqlServer());
 
             services.AddControllersWithViews();
 
